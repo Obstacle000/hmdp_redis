8a98598f3a83f50d9c97743f86c7b2d36c4a8c41
Obstacle000 <cwczsl12345@gmail.com>
2025-04-27 15:38:59 +0800

    自定义缓存工具类

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,18 +1,30 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+
 @SpringBootTest
 class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private CacheUtil cacheUtil;
+
     @Test
-    void contextLoads() throws InterruptedException {
-        shopService.saveShopToRedis(1L,10L);
+    void contextLoads() {
+        Shop shop = shopService.getById(1L);
+        cacheUtil.setWithLogicalExpire(CACHE_SHOP_KEY+1,shop,10L, TimeUnit.SECONDS);
     }
+
 
 }
