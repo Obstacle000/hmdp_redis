63a2bd4fdba58fdd94f77d8c62ee0c1cc81beef0
Obstacle000 <cwczsl12345@gmail.com>
2025-07-14 10:48:48 +0800

    连续签到功能

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -12,9 +12,12 @@ import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.UserHolder;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.TimeoutUtils;
@@ -23,7 +26,10 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -108,6 +114,60 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         stringRedisTemplate.expire(LOGIN_USER_KEY+token, LOGIN_USER_TTL, TimeUnit.MINUTES);
         // 7. 返回token
         return Result.ok(token);
+    }
+
+    @Override
+    public Result sign() {
+        Long userId = UserHolder.getUser().getId();
+        // 获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String str = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+
+        String key = USER_SIGN_KEY+userId+str;
+        //今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+
+        stringRedisTemplate.opsForValue().setBit(key,dayOfMonth-1,true);
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result signCount() {
+        Long userId = UserHolder.getUser().getId();
+        // 获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String str = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+
+        String key = USER_SIGN_KEY+userId+str;
+        //今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+
+        // 所有记录
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key, BitFieldSubCommands.create().get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+
+        if (result == null||result.isEmpty()) {
+            return Result.ok(0);
+        }
+        Long num = result.get(0);
+        if(num == null||num == 0) return Result.ok(0);
+        int count = 0;
+        while(true){
+            if((num&1) == 0){
+                // 意味着未签到,直接结束
+                break;
+            }else {
+                // 计算连续天数
+                count++;
+            }
+            // num右移
+            num = num>>1;
+        }
+        return Result.ok(count);
     }
 
     private User createUserWithPhone(String phone) {
